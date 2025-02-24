b90df36c304ed14395b8388405ffe6b81e9032ac
Pedro Bernardino <52471004+pedro-bernardino@users.noreply.github.com>
2025-02-24 21:51:48 +0000

    added a new project

--- src/Components/Projects/ProjectCard.jsx
+++ src/Components/Projects/ProjectCard.jsx
@@ -1,11 +1,11 @@
 import Technology from './ProjectCardTechnology'
 
-function ProjectCard({href, src, h3, tech}) {
+function ProjectCard({href, src, h3Type, h3Name, tech}) {
   return (
     <div className='card'>
-        <h3>{h3}</h3>
+        <h3>{h3Type} <br /> {h3Name}</h3>
         <a href={href} target="_blank" >
-            <img className='hover' src={src} alt={`${h3} logo`} />
+            <img className='hover' src={src} alt={`${h3Name} logo`} />
         </a>
         <Technology tech={tech} />
     </div>
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,13 +1,22 @@
 import styles from './Projects.module.css'
 
 import ProjectCard from '../Projects/ProjectCard'
+
+//projects images
 import PortfolioLogo from '../../assets/portfolio.png'
+import Kobodrop from '../../assets/kobodrop.png'
+
+//icons
 import techReact from '../../assets/react.svg'
 import techVite from '../../assets/vite.svg'
+import techTailwind from '../../assets/tailwind.svg'
+import techTypeScript from '../../assets/typescript.svg'
 
 function Projects() {
 const reactTech = {logo: techReact, name: 'React'}
 const viteTech = {logo: techVite, name: 'Vite'}
+const tailwindTech = {logo: techTailwind, name: 'Tailwind'}
+const typescriptTech = {logo: techTypeScript, name: 'TypeScript'}
 
   return (
     <section id='projects' className={styles.container}>
@@ -16,49 +25,19 @@ const viteTech = {logo: techVite, name: 'Vite'}
           <ProjectCard 
               href=''
               src={PortfolioLogo}
-              h3='Landing Page Portfolio'
+              h3Type='Landing Page'
+              h3Name='Portfolio'
               tech={
                 [reactTech, viteTech]
               }
           />
           <ProjectCard 
               href=''
-              src={PortfolioLogo}
-              h3='Landing Page Portfolio'
+              src={Kobodrop}
+              h3Type='Landing Page'
+              h3Name='Finantial App'
               tech={
-                [reactTech, viteTech]
-              }
-          />
-          <ProjectCard 
-              href=''
-              src={PortfolioLogo}
-              h3='Landing Page Portfolio'
-              tech={
-                [reactTech, viteTech]
-              }
-          />
-          <ProjectCard 
-              href=''
-              src={PortfolioLogo}
-              h3='Landing Page Portfolio'
-              tech={
-                [reactTech, viteTech]
-              }
-          />
-          <ProjectCard 
-              href=''
-              src={PortfolioLogo}
-              h3='Landing Page Portfolio'
-              tech={
-                [reactTech, viteTech]
-              }
-          />
-          <ProjectCard 
-              href=''
-              src={PortfolioLogo}
-              h3='Landing Page Portfolio'
-              tech={
-                [reactTech, viteTech]
+                [reactTech, viteTech, tailwindTech, typescriptTech]
               }
           />
         </div>
